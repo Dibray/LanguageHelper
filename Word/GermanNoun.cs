cb6ddb6b473fefbe8112eb5eb1f94c2de478a768
Vadim <mr.dimov2001@mail.ru>
2022-06-17 13:54:21 +0300

    German word changed

---
+++ Word/GermanNoun.cs
@@ -0,0 +1,34 @@
+﻿namespace LanguageHelper
+{
+    class GermanNoun : GermanWord
+    {
+        public enum DefiniteArticle : byte
+        {
+            Der = 0, Die, Das
+        }
+
+        public override string Value
+        {
+            get
+            {
+                return Article.ToString() + " " + base.Value;
+            }
+        }
+
+        private DefiniteArticle article;
+        private DefiniteArticle Article
+        {
+            get
+            {
+                return article;
+            }
+
+            set
+            {
+                article = value;
+            }
+        }
+
+        GermanNoun() { }
+    }
+}
--- Word/GermanWord.cs
+++ Word/GermanWord.cs
@@ -2,39 +2,9 @@
 {
     public class GermanWord : Word
     {
-        public enum DefiniteArticle : byte
-        {
-            Der = 0, Die, Das
-        }
-
-        public override string Value
-        {
-            get
-            {
-                return Article.ToString() + " " + base.Value;
-            }
-        }
-
-        private DefiniteArticle article;
-        private DefiniteArticle Article
-        {
-            get
-            {
-                return article;
-            }
-
-            set
-            {
-                article = value;
-            }
-        }
-
         public GermanWord() { }
 
-        public GermanWord(in string word, in DefiniteArticle article)
-            : base(word)
-        {
-            Article = article;
-        }
+        public GermanWord(in string word)
+            : base(word) { }
     }
 }
